429ebe885bcb1c7a63d37034340e7cfbb8e5b858
xu1211 <xu.yuchun@foxmail.com>
2022-07-21 17:18:58 +0800

    接口断言

---
+++ 语法/3函数/8.1类型断言.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/**
+空接口可以存储任意类型的值
+想要判断空接口中的值这个时候就可以使用类型断言
+语法格式： x.(T)
+    x：表示类型为interface{}的变量
+    T：表示断言x可能是的类型。
+*/
+
+type structTypes interface {
+	structTypes()
+}
+type stringStruct struct {
+	s string
+}
+
+func (*stringStruct) structTypes() {}
+
+type intStruct struct {
+	i int
+}
+
+func (*intStruct) structTypes() {}
+
+func main() {
+	// 测试1: interface{} 值为基础类型
+	var x interface{}
+	x = "string"
+	//x = 1
+
+	v, ok := x.(string)
+	if ok {
+		fmt.Println("x是string: " + v)
+	} else {
+		fmt.Println("string 类型断言失败")
+	}
+
+	i, ok := x.(int)
+	if ok {
+		fmt.Println("x是int: " + strconv.Itoa(i))
+	} else {
+		fmt.Println("int 类型断言失败")
+	}
+
+	// 测试2: interface{} 值为struct
+	alltype()
+}
+func alltype() {
+	var a structTypes
+	a = &stringStruct{s: "s"}
+	fmt.Println(a)
+	if v, ok := a.(*stringStruct); ok {
+		fmt.Println(v.s)
+	}
+
+	a = &intStruct{i: 123}
+	fmt.Println(a)
+	if v, ok := a.(*intStruct); ok {
+		fmt.Println(v.i)
+	}
+}
